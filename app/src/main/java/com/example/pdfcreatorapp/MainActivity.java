9d8381835df3fc612ec7e37523d597b14d009384
Mohamed Belhassen <mohamed.belhassen@gmail.com>
2022-02-13 12:35:57 +0100

    some code optimizations using helper functions applied to images and the first table

--- app/src/main/java/com/example/pdfcreatorapp/MainActivity.java
+++ app/src/main/java/com/example/pdfcreatorapp/MainActivity.java
@@ -32,9 +32,16 @@ import java.io.IOException;
 import java.io.OutputStream;
 
 /*
-    //Source: https://www.youtube.com/watch?v=nWbFXYN5EME
-    //Part 2 of this video: https://youtu.be/lD7BvdJM7MY
-    // Implemented by Mohamed Belhassen
+    Source:
+        -https://www.youtube.com/watch?v=nWbFXYN5EME
+        -https://youtu.be/lD7BvdJM7MY
+
+    Implemented by: Mohamed Belhassen
+    --> with some code optimizations using helper functions applied to images and the first table
+
+    If you like this app, I hope see your star :) for this project on Github:
+    https://github.com/mohamedbelhassen/export-invoice-as-pdf-file-using_itext-library-and-export-it-to-external-storage-android-app
+
  */
 
 public class MainActivity extends AppCompatActivity {
@@ -51,12 +58,41 @@ public class MainActivity extends AppCompatActivity {
 
     }
 
+    private Cell createEmptyCellWithNoBorder(){
+        return new Cell().add(new Paragraph("")).setBorder(Border.NO_BORDER);
+    }
+
+    private Cell createCellWithTextNoBorder(String text){
+        return new Cell().add(new Paragraph(text)).setBorder(Border.NO_BORDER);
+    }
+
+    private Cell createCellWithColAndRowSpanNoBorder( int colSpan, int rowSpan){
+        return new Cell(colSpan,rowSpan).setBorder(Border.NO_BORDER);
+    }
+
+    private Paragraph createParagraphWithFontSizeAndColor(String text, int fontSize, DeviceRgb color){
+        return new Paragraph(text)
+                    .setFontSize(fontSize)
+                    .setFontColor(color);
+    }
+
+    Image getImageFromDrawableResource(int drawableResourceId, int imageHeight){
+        Drawable d = getDrawable(drawableResourceId);
+        Bitmap bitmap = ((BitmapDrawable)d).getBitmap();
+        ByteArrayOutputStream stream= new ByteArrayOutputStream();
+        bitmap.compress(Bitmap.CompressFormat.PNG,100,stream);
+        byte[] bitmapData= stream.toByteArray();
+        ImageData imageData= ImageDataFactory.create(bitmapData);
+        Image image= new Image(imageData);
+        image.setHeight(imageHeight);
+        image.setHorizontalAlignment(HorizontalAlignment.RIGHT);
+        return image;
+    }
 
     private void createPDF() throws FileNotFoundException{
 
         String pdfPath = Environment.getExternalStoragePublicDirectory(Environment.DIRECTORY_DOWNLOADS).toString();
         File file = new File(pdfPath,"myPdf.pdf");
-        OutputStream outputStream= new FileOutputStream(file);
 
         PdfWriter writer= new PdfWriter(file);
         PdfDocument pdfDocument = new PdfDocument(writer);
@@ -70,68 +106,72 @@ public class MainActivity extends AppCompatActivity {
 
         Image image1=getImageFromDrawableResource(R.drawable.logo,120);
 
-
         //Table 1----------01
-        table1.setBorder(Border.NO_BORDER);
-        table1.addCell(new Cell(4,1).add(image1));
-        table1.addCell(new Cell().add(new Paragraph("")));
-        table1.addCell(new Cell(1,2).add(new Paragraph("Invoice")
-                                                            .setFontSize(26f)
-                                                            .setFontColor(invoiceGreen)));
+        table1.addCell(new Cell(4,1).add(image1).setBorder(Border.NO_BORDER));
+        table1.addCell(createEmptyCellWithNoBorder());
+        table1.addCell(
+                createCellWithColAndRowSpanNoBorder(1,2)
+                .add(createParagraphWithFontSizeAndColor("Invoice", 26,invoiceGreen))
+        );
+
         //table1.addCell(new Cell().add(new Paragraph("")));
 
         //Table 1----------02
         //table1.addCell(new Cell().add(new Paragraph("")));
-        table1.addCell(new Cell().add(new Paragraph("")));
-        table1.addCell(new Cell().add(new Paragraph("Invoice No:")));
-        table1.addCell(new Cell().add(new Paragraph("#2345678")));
+        table1.addCell(createEmptyCellWithNoBorder());
+        table1.addCell(createCellWithTextNoBorder("Invoice No:"));
+        table1.addCell(createCellWithTextNoBorder("#2345678"));
 
         //Table 1----------03
         //table1.addCell(new Cell().add(new Paragraph("")));
-        table1.addCell(new Cell().add(new Paragraph("")));
-        table1.addCell(new Cell().add(new Paragraph("Invoice Date:")));
-        table1.addCell(new Cell().add(new Paragraph("10-03-2021")));
+        table1.addCell(createEmptyCellWithNoBorder());
+        table1.addCell(createCellWithTextNoBorder("Invoice Date:"));
+        table1.addCell(createCellWithTextNoBorder("10-03-2021"));
 
         //Table 1----------04
         //table1.addCell(new Cell().add(new Paragraph("")));
-        table1.addCell(new Cell().add(new Paragraph("")));
-        table1.addCell(new Cell().add(new Paragraph("Account No:")));
-        table1.addCell(new Cell().add(new Paragraph("223344")));
+        table1.addCell(createEmptyCellWithNoBorder());
+        table1.addCell(createCellWithTextNoBorder("Account No:"));
+        table1.addCell(createCellWithTextNoBorder("223344"));
 
         //Table 1----------05
-        table1.addCell(new Cell().add(new Paragraph("\n")));
-        table1.addCell(new Cell().add(new Paragraph("")));
-        table1.addCell(new Cell().add(new Paragraph("")));
-        table1.addCell(new Cell().add(new Paragraph("")));
+        table1.addCell(createCellWithTextNoBorder("\n"));
+        table1.addCell(createEmptyCellWithNoBorder());
+        table1.addCell(createEmptyCellWithNoBorder());
+        table1.addCell(createEmptyCellWithNoBorder());
 
         //Table 1----------06
-        table1.addCell(new Cell().add(new Paragraph("To")));
-        table1.addCell(new Cell().add(new Paragraph("")));
-        table1.addCell(new Cell().add(new Paragraph("")));
-        table1.addCell(new Cell().add(new Paragraph("")));
+        table1.addCell(createCellWithTextNoBorder("To"));
+        table1.addCell(createEmptyCellWithNoBorder());
+        table1.addCell(createEmptyCellWithNoBorder());
+        table1.addCell(createEmptyCellWithNoBorder());
 
         //Table 1----------07
-        table1.addCell(new Cell().add(new Paragraph("Disha Verma")));
-        table1.addCell(new Cell().add(new Paragraph("")));
-        table1.addCell(new Cell().add(new Paragraph("Payment method:").setBold()));
-        table1.addCell(new Cell().add(new Paragraph("")));
+        table1.addCell(createCellWithTextNoBorder("Disha Verma"));
+        table1.addCell(createEmptyCellWithNoBorder());
+        table1.addCell(createCellWithTextNoBorder("Payment method:").setBold());
+        table1.addCell(createEmptyCellWithNoBorder());
 
         //Table 1----------08
-        table1.addCell(new Cell().add(new Paragraph("523 Andheri west")));
-        table1.addCell(new Cell().add(new Paragraph("")));
-        table1.addCell(new Cell(1,2).add(new Paragraph("Paypal: payments@greenleaf.com")));
+        table1.addCell(createCellWithTextNoBorder("523 Andheri west"));
+        table1.addCell(createEmptyCellWithNoBorder());
+        table1.addCell(
+                createCellWithColAndRowSpanNoBorder(1,2)
+                        .add(new Paragraph("Paypal: payments@greenleaf.com"))
+        );
         //table1.addCell(new Cell().add(new Paragraph("")));
 
         //Table 1----------09
-        table1.addCell(new Cell().add(new Paragraph("Mumbai")));
-        table1.addCell(new Cell().add(new Paragraph("")));
-        table1.addCell(new Cell(1,2).add(new Paragraph("Card payment: We accept Visa, Mastercard")));
+        table1.addCell(createCellWithTextNoBorder("Mumbai"));
+        table1.addCell(createCellWithTextNoBorder(""));
+        table1.addCell(
+                createCellWithColAndRowSpanNoBorder(1,2)
+                        .add(new Paragraph("Card payment: We accept Visa, Mastercard"))
+        );
         //table1.addCell(new Cell().add(new Paragraph("")));
 
         float columnWidth2[]={62,162,112,112,112};
         Table table2 = new Table(columnWidth2);
-
-
 
         //table2----------01
         table2.addCell(new Cell().add(new Paragraph("S. No").setFontColor(ColorConstants.WHITE)).setBackgroundColor(invoiceGreen));
@@ -146,6 +186,7 @@ public class MainActivity extends AppCompatActivity {
         table2.addCell(new Cell().add(new Paragraph("50")).setBackgroundColor(invoiceGray));
         table2.addCell(new Cell().add(new Paragraph("2")).setBackgroundColor(invoiceGray));
         table2.addCell(new Cell().add(new Paragraph("100")).setBackgroundColor(invoiceGray));
+
         //table2----------03
         table2.addCell(new Cell().add(new Paragraph("2.")).setBackgroundColor(invoiceGray));
         table2.addCell(new Cell().add(new Paragraph("Furtilizer")).setBackgroundColor(invoiceGray));
@@ -188,15 +229,12 @@ public class MainActivity extends AppCompatActivity {
         table2.addCell(new Cell().add(new Paragraph("Grand Total").setBold().setFontSize(16).setFontColor(ColorConstants.WHITE)).setBackgroundColor(invoiceGreen));
         table2.addCell(new Cell().add(new Paragraph("1232").setBold().setFontSize(16).setFontColor(ColorConstants.WHITE)).setBackgroundColor(invoiceGreen));
 
-
         float columnWidth3[]={50,250,260};
         Table table3 = new Table(columnWidth3);
 
         Image image2=getImageFromDrawableResource(R.drawable.contact,120);
         Image image3=getImageFromDrawableResource(R.drawable.thankyou,120);
         image3.setHorizontalAlignment(HorizontalAlignment.RIGHT);
-
-
 
         table3.addCell(new Cell(3,1).add(image2).setBorder(Border.NO_BORDER));
         table3.addCell(new Cell().add(new Paragraph("yourEmail@gmail.com\n" +
@@ -206,9 +244,6 @@ public class MainActivity extends AppCompatActivity {
         table3.addCell(new Cell().add(new Paragraph("+91-11234345677\n+91-11234345678")).setBorder(Border.NO_BORDER));
         table3.addCell(new Cell().add(new Paragraph("#153/A Mahraja Street\nGwalior, India")).setBorder(Border.NO_BORDER));
 
-        //suite
-        //Part 2 of this video: https://youtu.be/lD7BvdJM7MY
-
         document.add(table1);
         document.add(new Paragraph("\n"));
         document.add(table2);
@@ -217,19 +252,6 @@ public class MainActivity extends AppCompatActivity {
 
         document.close();
         Toast.makeText(this,"Pdf created", Toast.LENGTH_LONG).show();
-    }
-
-    Image getImageFromDrawableResource(int drawableResourceId, int imageHeight){
-        Drawable d = getDrawable(drawableResourceId);
-        Bitmap bitmap = ((BitmapDrawable)d).getBitmap();
-        ByteArrayOutputStream stream= new ByteArrayOutputStream();
-        bitmap.compress(Bitmap.CompressFormat.PNG,100,stream);
-        byte[] bitmapData= stream.toByteArray();
-        ImageData imageData= ImageDataFactory.create(bitmapData);
-        Image image= new Image(imageData);
-        image.setHeight(imageHeight);
-        image.setHorizontalAlignment(HorizontalAlignment.RIGHT);
-        return image;
     }
 
 }
